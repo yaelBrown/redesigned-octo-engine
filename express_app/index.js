a4b917128f6c1127c1e4d3fcb9b97e36980ee625
yaelBrown <yaelrbrown@gmail.com>
2023-05-06 21:17:06 -0500

    add: helmet and compression package

--- express_app/index.js
+++ express_app/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const path = require('path');
+const path = require('path')
+const helmet = require('helmet')
+const compression = require('compression')
 // const session = require('express-session');
 const app = express()
 const sequelize = require('./config/connection')
 
-const port = 8080
+const port = process.env.PORT || 8080
 const hbs = exphbs.create()
 
 app.engine('handlebars', hbs.engine)
@@ -14,6 +16,8 @@ app.set('view engine', 'handlebars')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet())
+app.use(compression())
 
 app.use(require('./routes/'));
 
@@ -24,3 +28,10 @@ app.use(require('./routes/'));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// Finish setting up models
+// Create placeholder page for creating posts (dev only)
+// Figure out deployment
+// Add gtag, fb pixel to page
+// Begin replicating the layout
