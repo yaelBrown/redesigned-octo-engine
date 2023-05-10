835f5efe417f9c21a04915a8e019c3c804d79135
yaelBrown <yaelrbrown@gmail.com>
2023-05-09 20:55:43 -0500

    chore: update create table posts in migration

--- db/db_schema_aws_rds.sql
+++ db/db_schema_aws_rds.sql
@@ -1,10 +1,14 @@
 DROP TABLE IF EXISTS blog_db;
 
 CREATE TABLE `posts` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `title` varchar(255) UNIQUE,
+  `title` varchar(255) DEFAULT NULL,
   `summary` varchar(255) DEFAULT NULL,
-  `author` varchar(100),
-  `content` text
-) ENGINE=InnoDB AUTO_INCREMENT=3306 DEFAULT CHARSET=utf8mb4;
+  `author` varchar(100) DEFAULT NULL,
+  `content` text,
+  `post_type` enum('money','crypto','finEd') DEFAULT 'money',
+  `image` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=InnoDB AUTO_INCREMENT=3306 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
