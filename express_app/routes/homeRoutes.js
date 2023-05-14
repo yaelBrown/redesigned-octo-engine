b21c01154d9484d581343c3f5a433689f47915f2
yaelBrown <yaelrbrown@gmail.com>
2023-05-14 10:24:53 -0500

    feat: added search and category template pages

--- express_app/routes/homeRoutes.js
+++ express_app/routes/homeRoutes.js
@@ -8,4 +8,32 @@ router.get('/', async (req, res) => {
   res.render('home', {posts})
 })
 
+router.get('/search', (req, res) => {
+  res.render('search')
+})
+
+// router.post('search') 
+// get query, pull from db, return results 
+
+router.get('/finance', (req, res) => {
+  const data = {
+    cat: 'finance'
+  }
+  res.render('category', {data})
+})
+
+router.get('/crypto', (req, res) => {
+  const data = {
+    cat: 'crypto'
+  }
+  res.render('category', {data})
+})
+
+router.get('/finEd', (req, res) => {
+  const data = {
+    cat: 'finEd'
+  }
+  res.render('category', {data})
+})
+
 module.exports = router
