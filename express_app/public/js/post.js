751d70d768c3dce8f54d41c8964779c3e864ec45
yaelBrown <yaelrbrown@gmail.com>
2023-05-22 03:29:51 -0500

    rem: post.js

--- express_app/public/js/post.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const content = document.getElementById("content")
-//   const dbstr = document.getElementById("dbstr").innerText
-  
-//   content.innerHtml = dbstr.innerText
-// })
-
-// I dont need this
